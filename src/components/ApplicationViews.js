a6548a9bec2b174a36e8cec23bfc10daac79e965
estherviolin <esthernaomi.violin@gmail.com>
2020-09-03 15:59:41 -0500

    almost done locations nav bar

---
+++ src/components/ApplicationViews.js
@@ -0,0 +1,24 @@
+import React from "react"
+import { Route } from "react-router-dom"
+import { LocationProvider } from "./location/LocationProvider"
+import { LocationList } from "./location/LocationList"
+import { ProductProvider } from "./product/ProductProvider"
+import { ProductList } from "./product/ProductList"
+
+export const ApplicationViews = (props) => {
+    return (
+        <>
+            <LocationProvider>
+                <Route exact path="/">
+                    <LocationList />
+                </Route>
+            </LocationProvider>
+
+            <ProductProvider>
+                <Route path="/products">
+                    <ProductList />
+                </Route>
+            </ProductProvider>
+        </>
+    )
+}
