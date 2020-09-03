f645ac16e9208627bbb2d4833147165cf5444182
estherviolin <esthernaomi.violin@gmail.com>
2020-09-03 13:16:28 -0500

    product type fixed

--- src/components/product/Product.js
+++ src/components/product/Product.js
@@ -4,6 +4,6 @@ export const Product = ({product}) => (
     <section className="product">
         <h3 className="product__name">{product.name}</h3>
         <div className="product__price">{product.price}</div>
-        <div className="product__type">Product Type: {product.typeId}</div>
+        <div className="product__type">Product Type: {product.productTypeId}</div>
     </section>
 )
